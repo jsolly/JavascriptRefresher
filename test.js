618c94bcb8f308d4747851c5fb638b170c924da4
John Solly <jsolly@pm.me>
2021-10-26 10:22:50 -0700

    WIP

---
+++ test.js
@@ -0,0 +1,8 @@
+var likesMeat = false;
+
+if (likesMeat){
+    document.write("Here is the meat menu")
+} else{
+    document.write("Here is the vegetarian menu")
+}
+//todo Look into JavaScript autoformatter
